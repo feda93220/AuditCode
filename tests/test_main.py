6f035733fe4b0bf175d2a10791f522453012e8d1
feda93220 <147146434+feda93220@users.noreply.github.com>
2024-06-11 12:26:40 +0200

    def main + test_main

---
+++ tests/test_main.py
@@ -0,0 +1,16 @@
+import unittest
+from src.main import add
+
+class TestMain(unittest.TestCase):
+    def test_add(self):
+         # Test avec les valeurs 2 et 4
+        self.assertEqual(add(2, 4), 6)
+        
+        # Test avec les valeurs -1 et 3
+        self.assertEqual(add(-1, 3), 2)
+        
+        # Test avec les valeurs -1 et -2
+        self.assertEqual(add(-1, -2), -3)
+
+if __name__== '__main__':
+    unittest.main()
